60479cf18575ac881a2653bf7ad486d14a3ca2ca
Tal Kohavy <talkohavy@gmail.com>
2023-12-08 19:46:13 +0200

    added test for addItemToList

---
+++ __test__/addItemToList.test.js
@@ -0,0 +1,41 @@
+import assert from 'assert/strict';
+import { describe, it } from 'node:test';
+import { addItemToList } from '../lib/addItemToList.js';
+
+describe('addItemToList', () => {
+  it('adds 1 item to array', () => {
+    const newItem = { a: 'John' };
+    const oldArr = ['Sarah', 'Nicky', 'Don'];
+
+    const newArr = addItemToList({ list: oldArr, newItem });
+
+    const actual = newArr.at(-1);
+    const expected = newItem;
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it('adding does not mutate the original array', () => {
+    const newItem = { a: 'John' };
+    const oldArr = ['Sarah', 'Nicky', 'Don'];
+
+    const newArr = addItemToList({ list: oldArr, newItem });
+
+    const actual = oldArr;
+    const expected = newArr;
+
+    assert.notStrictEqual(actual, expected);
+  });
+
+  it("adding an item increases array's length by +1", () => {
+    const newItem = { a: 'John' };
+    const oldArr = ['Sarah', 'Nicky', 'Don'];
+
+    const newArr = addItemToList({ list: oldArr, newItem });
+
+    const actual = oldArr.length + 1;
+    const expected = newArr.length;
+
+    assert.strictEqual(actual, expected);
+  });
+});
