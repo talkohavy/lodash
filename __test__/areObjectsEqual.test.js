1b9e18a105deaf5d9df1c69dbe0cc98257288e61
Tal Kohavy <talkohavy@gmail.com>
2023-12-09 10:56:10 +0200

    added areObjectsEqual test

---
+++ __test__/areObjectsEqual.test.js
@@ -0,0 +1,97 @@
+import assert from 'assert/strict';
+import { describe, it } from 'node:test';
+import { areObjectsEqual } from '../lib/areObjectsEqual.js';
+
+describe('areObjectsEqual', () => {
+  describe('same pointer', () => {
+    it('two empty objects should return true', () => {
+      const obj1 = {};
+      const obj2 = obj1;
+
+      const actual = areObjectsEqual(obj1, obj2);
+      const expected = true;
+
+      assert.deepStrictEqual(obj1, obj2);
+      assert.strictEqual(actual, expected);
+    });
+
+    it('two non-empty objects with should return true', () => {
+      const obj1 = { foo: 'bar', name: 'John' };
+      const obj2 = obj1;
+
+      const actual = areObjectsEqual(obj1, obj2);
+      const expected = true;
+
+      assert.deepStrictEqual(obj1, obj2);
+      assert.strictEqual(actual, expected);
+    });
+
+    it('two empty arrays should return true', () => {
+      const obj1 = [];
+      const obj2 = obj1;
+
+      const actual = areObjectsEqual(obj1, obj2);
+      const expected = true;
+
+      assert.deepStrictEqual(obj1, obj2);
+      assert.strictEqual(actual, expected);
+    });
+
+    it('two non-empty arrays should return true', () => {
+      const obj1 = ['foo', { hello: 'world' }];
+      const obj2 = obj1;
+
+      const actual = areObjectsEqual(obj1, obj2);
+      const expected = true;
+
+      assert.deepStrictEqual(obj1, obj2);
+      assert.strictEqual(actual, expected);
+    });
+  });
+
+  describe('different pointers', () => {
+    it('two empty objects should return true', () => {
+      const obj1 = {};
+      const obj2 = {};
+
+      const actual = areObjectsEqual(obj1, obj2);
+      const expected = true;
+
+      assert.deepStrictEqual(obj1, obj2);
+      assert.strictEqual(actual, expected);
+    });
+
+    it('two same-valued ordered objects should return true', () => {
+      const obj1 = { foo: 'bar', name: 'John' };
+      const obj2 = { foo: 'bar', name: 'John' };
+
+      const actual = areObjectsEqual(obj1, obj2);
+      const expected = true;
+
+      assert.deepStrictEqual(obj1, obj2);
+      assert.strictEqual(actual, expected);
+    });
+
+    it('two same-valued unordered objects should return true', () => {
+      const obj1 = { name: 'John', foo: 'bar' };
+      const obj2 = { foo: 'bar', name: 'John' };
+
+      const actual = areObjectsEqual(obj1, obj2);
+      const expected = true;
+
+      assert.deepStrictEqual(obj1, obj2);
+      assert.strictEqual(actual, expected);
+    });
+
+    it('two different-valued objects should return false', () => {
+      const obj1 = { name: 'John', foo: 'bar', field: 'new field' };
+      const obj2 = { foo: 'bar', name: 'John' };
+
+      const actual = areObjectsEqual(obj1, obj2);
+      const expected = false;
+
+      assert.notDeepStrictEqual(obj1, obj2);
+      assert.strictEqual(actual, expected);
+    });
+  });
+});
