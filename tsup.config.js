d5c4cf981b19db39235d5dd33217c76259314230
Tal Kohavy <talkohavy@gmail.com>
2023-12-09 09:22:41 +0200

    tsup build now creates its own package.json

--- tsup.config.js
+++ tsup.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'tsup';
 import fs from 'fs';
+import { defineConfig } from 'tsup';
 
 // The options here is derived from CLI flags.
-export default defineConfig((options) => ({
+export default defineConfig((_options) => ({
   entry: ['lib/index.js'],
   publicDir: './lib',
   outDir: 'dist', // <--- defaults to dist
@@ -17,13 +17,23 @@ export default defineConfig((options) => ({
   // target: , // <--- The value for target defaults to compilerOptions.target in your tsconfig.json, or node14 if unspecified. For more information check out esbuild's target option.
   // env: process.NODE_ENV,
   async onSuccess() {
-    const readStreamPackageJson = fs.createReadStream('./package.json');
-    const writeStreamPackageJson = fs.createWriteStream('./dist/package.json');
+    // Step 1: copy README file as-is to dist
     const readStreamReadmeMd = fs.createReadStream('./README.md');
     const writeStreamReadmeMd = fs.createWriteStream('./dist/README.md');
-
-    readStreamPackageJson.pipe(writeStreamPackageJson);
     readStreamReadmeMd.pipe(writeStreamReadmeMd);
+
+    // Step 2: get the package.json file
+    const packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
+
+    // Step 3: Remove all scripts
+    delete packageJson.scripts;
+
+    // Step 4: Change from private to public
+    packageJson.private = false;
+    packageJson.publishConfig.access = 'public';
+
+    // Step 5: create new package.json file in dist
+    fs.writeFileSync('./dist/package.json', JSON.stringify(packageJson));
 
     console.log('DONE !!!');
     // return () => { cleanupFunction() }
