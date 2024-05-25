9c479e3175d770551e8d562a765671d5fe93258d
Tal Kohavy <talkohavy@gmail.com>
2024-05-25 22:39:14 +0300

    going back to previous method of doing cd dist

--- toolbox/postBuild.js
+++ toolbox/postBuild.js
@@ -10,6 +10,10 @@ postBuild();
 async function postBuild() {
   generateNewIndexDtsFile();
 
+  copyReadmeFile();
+
+  copyAndManipulatePackageJsonFile();
+
   console.log('DONE !!!');
 }
 
@@ -37,4 +41,35 @@ function generateNewIndexDtsFile() {
   fs.writeFileSync(`${outFolderName}/index.d.ts`, updatedIndexDts);
 
   console.log('-- Generated new index.d.ts file successfully!');
+}
+
+/**
+ * @description
+ * Copy README file as-is to the output folder.
+ */
+function copyReadmeFile() {
+  console.log('- Step 3: copy the README.md file');
+  const readStreamReadmeMd = fs.createReadStream('./README.md');
+  const writeStreamReadmeMd = fs.createWriteStream(`./${outFolderName}/README.md`);
+  readStreamReadmeMd.pipe(writeStreamReadmeMd);
+}
+
+function copyAndManipulatePackageJsonFile() {
+  console.log('- Step 4: copy & manipulate the package.json file');
+  // Step 1: get the original package.json file
+  const packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
+
+  // Step 2: Remove all scripts
+  delete packageJson.scripts;
+  console.log('-- deleted `scripts` key');
+
+  // Step 3: Change from private to public
+  delete packageJson.private;
+  packageJson.publishConfig.access = 'public';
+  console.log('-- changed from private to public');
+  console.log('-- changed publishConfig access to public');
+
+  // Step 4: create new package.json file in the output folder
+  fs.writeFileSync(`./${outFolderName}/package.json`, JSON.stringify(packageJson));
+  console.log('-- package.json file written successfully!');
 }
