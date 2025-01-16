ff6594c53fd2c14fa88ff796055de96f036908bc
Tal Kohavy <talkohavy@gmail.com>
2025-01-16 06:44:34 +0200

    added a default type to removeItemFromListByIndex

--- src/removeItemFromListByIndex.ts
+++ src/removeItemFromListByIndex.ts
@@ -4,7 +4,7 @@ type RemoveItemFromListByIndexProps<T> = {
 };
 
 // TODO: make this toSpliced when you move to node 20
-export function removeItemFromListByIndex<T>(props: RemoveItemFromListByIndexProps<T>): Array<T> {
+export function removeItemFromListByIndex<T = any>(props: RemoveItemFromListByIndexProps<T>): Array<T> {
   const { list, index } = props;
 
   const newList = [...list];
