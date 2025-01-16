09ab1771f4ed8f098afa67b24b4310027a2f7e7f
Tal Kohavy <talkohavy@gmail.com>
2025-01-16 06:40:14 +0200

    added a default type to addItemToList

--- src/addItemToList.ts
+++ src/addItemToList.ts
@@ -1,6 +1,9 @@
-type addItemToListProps<T> = { list: Array<T>; newItem: T };
+type addItemToListProps<T> = {
+  list: Array<T>;
+  newItem: T;
+};
 
-export function addItemToList<T>(props: addItemToListProps<T>) {
+export function addItemToList<T = any>(props: addItemToListProps<T>) {
   const { list, newItem } = props;
 
   return list.concat(newItem);
